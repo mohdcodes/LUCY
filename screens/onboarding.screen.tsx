2fc657289d96a70a34e55c94b50682d0bd9b4901
ARBAAZcode <arbaazsid0786@gmail.com>
2024-10-08 11:45:29 +0530

    PAGINATION , SKIP BUTTON (not working) : ✅

--- screens/onboarding.screen.tsx
+++ screens/onboarding.screen.tsx
@@ -7,11 +7,14 @@ import {
   NativeSyntheticEvent,
   NativeScrollEvent,
   Dimensions,
+  ViewBase,
 } from 'react-native';
+import AntDesign from '@expo/vector-icons/AntDesign';
 import React, { useRef, useState } from 'react';
 import { LinearGradient } from 'expo-linear-gradient';
 import { onBoardingData } from '@/configs/constants';
-import { scale } from 'react-native-size-matters';
+import { scale, verticalScale } from 'react-native-size-matters';
+import index from '@/app';
 
 export default function OnBoardingScreen() {
   const [activeIndex, setActiveIndex] = useState(0);
@@ -24,13 +27,21 @@ export default function OnBoardingScreen() {
     setActiveIndex(currentIndex);
   };
   return (
+    // Linear Gradient Background.
     <LinearGradient
       colors={['#150252', '#00fff0']}
       start={{ x: 0, y: 0 }}
       end={{ x: 1, y: 1 }}
       style={styles.container}
     >
-      <StatusBar barStyle={'light-content'} />
+      {/* STATUS BAR HIDDEN */}
+      <StatusBar barStyle={'light-content'} hidden />
+      {/* Skip on the Top Right */}
+      <View style={styles.skiContainer}>
+        <Text style={styles.skipText}>skip</Text>
+        <AntDesign name="arrowright" size={scale(18)} color="white" />
+      </View>
+      {/* Horizontal Scroll View */}
       <ScrollView
         horizontal
         pagingEnabled
@@ -45,10 +56,19 @@ export default function OnBoardingScreen() {
           </View>
         ))}
       </ScrollView>
+      {/* Pagination */}
+      <View style={styles.paginationContainer}>
+        {onBoardingData.map((_, index) => (
+          <View
+            key={index}
+            style={[styles.dot, { opacity: activeIndex === index ? 1 : 0.3 }]}
+          />
+        ))}
+      </View>
     </LinearGradient>
   );
 }
-
+// StyleSheet
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -74,5 +94,37 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     marginTop: 8,
     marginHorizontal: 5,
+  },
+  paginationContainer: {
+    position: 'absolute',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    bottom: verticalScale(50),
+    alignItems: 'center',
+    gap: scale(8),
+  },
+  dot: {
+    width: scale(8),
+    height: scale(8),
+    borderRadius: 100,
+    backgroundColor: '#ffffff',
+    marginHorizontal: scale(2),
+  },
+  skiContainer: {
+    marginTop: 20,
+    position: 'absolute',
+    top: verticalScale(20),
+    right: scale(20),
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: scale(5),
+  },
+  skipText: {
+    color: '#ffffff',
+    fontSize: scale(14),
+    fontWeight: '400',
+  },
+  arrowDesign: {
+    marginLeft: 10,
   },
 });
